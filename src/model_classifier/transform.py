675c81cdc00baa5aa51f64b86c11476675cb8a40
Arash Hashemi <arashhashemi.1995.74@gmail.com>
2024-01-28 08:44:10 -0500

    updated the transform script, removed data_prep.py

--- src/model_classifier/transform.py
+++ src/model_classifier/transform.py
@@ -8,7 +8,6 @@ def transform_image():
   mean, std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
   custom_to_dtype = transforms.Lambda(lambda x: transforms.ToTensor()(x).to(torch.float32))
   image_transforms = transforms.Compose([
-    # v2.ToPILImage(),
     transforms.Resize((400,400), antialias=True),
     transforms.RandomCrop(350),
     transforms.RandomHorizontalFlip(0.5),
